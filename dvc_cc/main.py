54940c1e29a0ca6f02dce08f7077170d2e4090c2
Jonas Annuscheit <annusch@htw-berlin.de>
2019-05-22 20:52:34 +0200

    use "dvc-cc run" instead of "dvc-cc jobs run"

--- dvc_cc/main.py
+++ dvc_cc/main.py
@@ -7,8 +7,8 @@ from dvc_cc.cli_modes import cli_modes
 from dvc_cc.git.main import main as git_main
 from dvc_cc.git.main import DESCRIPTION as GIT_DESCRIPTION
 
-from dvc_cc.jobs.main import main as jobs_main
-from dvc_cc.jobs.main import DESCRIPTION as JOBS_DESCRIPTION
+from dvc_cc.run.main import main as run_main
+from dvc_cc.run.main import DESCRIPTION as run_DESCRIPTION
 
 from dvc_cc.init.main import main as init_main
 from dvc_cc.init.main import DESCRIPTION as INIT_DESCRIPTION
@@ -18,7 +18,7 @@ TITLE = 'tools'
 DESCRIPTION = 'DVC-CC (C) 2019  Jonas Annuscheit. This software is distributed under the AGPL-3.0 LICENSE.'
 MODES = OrderedDict([
     ('git', {'main': git_main, 'description': GIT_DESCRIPTION}),
-    ('jobs', {'main': jobs_main, 'description': JOBS_DESCRIPTION}),
+    ('run', {'main': run_main, 'description': run_DESCRIPTION}),
     ('init', {'main': init_main, 'description': INIT_DESCRIPTION})
 ])
 
--- dvc_cc/run/__main__.py
+++ dvc_cc/run/__main__.py
@@ -1,4 +1,4 @@
-from dvc_cc.job.main import main
+from dvc_cc.run.main import main
 
 
 if __name__ == '__main__':
