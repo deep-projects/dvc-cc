3ab1af2e116beecb7da88a3499a9c952c5ed23b2
Jonas <jonas.annuscheit@gmail.com>
2019-06-13 04:26:33 +0200

    modify the test script

--- dvc-cc-local/tests/test_script4.sh
+++ dvc-cc-local/tests/test_script4.sh
@@ -202,13 +202,21 @@ cd ~/test_pcam/repo
 git add -A
 git commit -m 'add train.ipynb'
 
-# RUN ONE
+# RUN ONE - WITHOUT HAVING THE DOWNLOADED DATA IN THE CACHE !!!
 dvc run --no-exec -d data/camelyonpatch_level_2_split_train_x.h5 -d data/camelyonpatch_level_2_split_train_y.h5 -d data/camelyonpatch_level_2_split_valid_x.h5 -d data/camelyonpatch_level_2_split_valid_y.h5 -o tf_models/tf_model.h5 -o tensorboards/tb -o outputs/all-history.json -o outputs/history-summary.json -f dvc/train_network.dvc --overwrite-dvcfile python code/train.py -lr 0.2
 
 git add dvc/train_network.dvc
 
 dvc-cc run -nb 'try_learning_rate_0.2'
 
+# push the data to the dvc cache
+dvc push
+# RUN ONE - NOW WITH DATA IN CACHE
+dvc run --no-exec -d data/camelyonpatch_level_2_split_train_x.h5 -d data/camelyonpatch_level_2_split_train_y.h5 -d data/camelyonpatch_level_2_split_valid_x.h5 -d data/camelyonpatch_level_2_split_valid_y.h5 -o tf_models/tf_model.h5 -o tensorboards/tb -o outputs/all-history.json -o outputs/history-summary.json -f dvc/train_network.dvc --overwrite-dvcfile python code/train.py -lr 0.2
+
+git add dvc/train_network.dvc
+
+dvc-cc run -r 10 -nb 'try_learning_rate_0.2'
 
 
 
@@ -218,7 +226,7 @@ dvc run --no-exec -d data/camelyonpatch_level_2_split_train_x.h5 -d data/camelyo
 
 git add dvc/train_network.dvc
 
-dvc-cc run -nb 'try_learning_rate_0.5'
+dvc-cc run -r 10 -nb 'try_learning_rate_0.5'
 
 
 
@@ -228,7 +236,7 @@ dvc run --no-exec -d data/camelyonpatch_level_2_split_train_x.h5 -d data/camelyo
 
 git add dvc/train_network.dvc
 
-dvc-cc run -nb 'try_learning_rate_0.1'
+dvc-cc run -r 10 -nb 'try_learning_rate_0.1'
 
 
 
@@ -240,7 +248,7 @@ dvc run --no-exec -d data/camelyonpatch_level_2_split_train_x.h5 -d data/camelyo
 
 git add dvc/train_network.dvc
 
-dvc-cc run -nb 'try_learning_rate_0.05'
+dvc-cc run -r 10 -nb 'try_learning_rate_0.05'
 
 
 
@@ -252,7 +260,7 @@ dvc run --no-exec -d data/camelyonpatch_level_2_split_train_x.h5 -d data/camelyo
 
 git add dvc/train_network.dvc
 
-dvc-cc run -nb 'try_learning_rate_0.01'
+dvc-cc run -r 10 -nb 'try_learning_rate_0.01'
 
 
 
