3d1fde8fae198d37f31f0a651fb00d6d5e501e00
Jonas Annuscheit <annusch@htw-berlin.de>
2019-06-19 17:12:56 +0200

    update the git commands to use always dvc checkout

--- dvc-cc/dvc_cc/git/main.py
+++ dvc-cc/dvc_cc/git/main.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 from dvc_cc.version import VERSION
 from dvc_cc.cli_modes import cli_modes
-
+import sys
 from argparse import ArgumentParser
 
 
@@ -13,19 +13,43 @@ from dvc_cc.git.commit_and_push import DESCRIPTION as COMMIT_AND_PUSH_DESCRIPTIO
 from dvc_cc.git.sync import main as sync_main
 from dvc_cc.git.sync import DESCRIPTION as SYNC_DESCRIPTION
 
+import subprocess
+from subprocess import check_output
+
 #from dvc_cc.job.main import main as job_main
 #from dvc_cc.job.main import DESCRIPTION as JOB_DESCRIPTION
 
-SCRIPT_NAME = 'dvc-cc'
+SCRIPT_NAME = 'dvc-cc git'
 TITLE = 'tools'
-DESCRIPTION = 'Scripts to make working with git easier.'
-MODES = OrderedDict([
-    #('commit-and-push', {'main': commit_and_push_main, 'description': COMMIT_AND_PUSH_DESCRIPTION}),
-    #('branch', {'main': git_branch, 'description': GIT_BRANCH_DESCRIPTION}),
-    ('sync', {'main': sync_main, 'description': SYNC_DESCRIPTION}),
-    #('checkout', {'main': red_main, 'description': RED_DESCRIPTION})
-])
+DESCRIPTION = 'This code call the git command and run after this "dvc checkout". If you call "dvc-cc git branch", it will not show the dvc-cc result branches. If you call "dvc-cc git sync" it will checkout to all remote branches!'
+
+
+def get_name_of_branch():
+    out = check_output(["git", "branch"]).decode("utf8")
+    current = next(line for line in out.split("\n") if line.startswith("*"))
+    return current.strip("*").strip()
 
 
 def main():
-    cli_modes(SCRIPT_NAME, TITLE, DESCRIPTION, MODES, VERSION)
+    argv = sys.argv[1:]
+    if len(argv) == 1 and sys.argv[1] == 'branch':
+        git_branch = check_output(['git','branch']).decode("utf8").split('\n')
+        for line in git_branch:
+            if not line.startswith('  bcc_') and not line.startswith('  remotes/origin/bcc_'):
+                print(line)
+    elif len(argv) == 1 and sys.argv[1] == 'sync':
+        git_name_of_branch = get_name_of_branch()
+        all_branches = check_output(["git", "branch", '-a']).decode("utf8").split("\n")
+        for b in all_branches:
+            pos = b.find('remotes/origin/')
+            if pos >= 0:
+                b = b[pos+15:]
+                print('git checkout '+ b)
+                _ = check_output(['git', 'checkout', b])
+                _ = check_output(['dvc', 'checkout'])
+        print('git checkout ' + git_name_of_branch)
+        _ = check_output(['git', 'checkout', git_name_of_branch])
+        _ = check_output(['dvc', 'checkout'])
+    else:
+        subprocess.call(['git'] + argv)
+        subprocess.call(['dvc', 'checkout'])
