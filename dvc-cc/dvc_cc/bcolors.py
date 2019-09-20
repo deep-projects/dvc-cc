194be62757e3a4b2b8b8033b087a4e158bb8aede
Jonas Annuscheit <annusch@htw-berlin.de>
2019-09-20 09:56:58 +0200

    ignore color visualization on windows

--- dvc-cc/dvc_cc/bcolors.py
+++ dvc-cc/dvc_cc/bcolors.py
@@ -1,9 +1,11 @@
+import os
+
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = '\033[95m' if os.name!='nt' else ''
+    OKBLUE = '\033[94m' if os.name!='nt' else ''
+    OKGREEN = '\033[92m' if os.name!='nt' else ''
+    WARNING = '\033[93m' if os.name!='nt' else ''
+    FAIL = '\033[91m' if os.name!='nt' else ''
+    ENDC = '\033[0m' if os.name!='nt' else ''
+    BOLD = '\033[1m' if os.name!='nt' else ''
+    UNDERLINE = '\033[4m' if os.name!='nt' else ''
