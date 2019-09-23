fa33a1b073a3e7aa2ae747a694fbd94f5066b7d2
Jonas Annuscheit <annusch@htw-berlin.de>
2019-09-24 01:41:17 +0200

    fix status

--- dvc-cc/dvc_cc/status/main.py
+++ dvc-cc/dvc_cc/status/main.py
@@ -240,7 +240,7 @@ def main():
     if args.show_ids == False:
         df['experimentId'] = '...'+df['experimentId'].apply(lambda x: x[-3:])
         df['_id'] = '...'+df['_id'].apply(lambda x: str(x)[-3:])
-    df['registrationTime'] = df['registrationTime'].apply(lambda x: time.ctime(int(x)))
+    df['registrationTime'] = df['registrationTime'].apply(lambda x: time.ctime(int(x)) if x==x else None)
     df['state'] = df['state'].replace(['succeeded', 'cancelled','failed','processing'],[bcolors.OKGREEN+'succeeded'+bcolors.ENDC,bcolors.WARNING+'cancelled'+bcolors.ENDC,bcolors.FAIL+'failed'+bcolors.ENDC,bcolors.OKBLUE+'processing'+bcolors.ENDC])
 
     if args.detail_unchanged:
